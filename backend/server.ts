3a6a0cd8ae2770af98fac86bb06a2d5bfb01e883
Eon S. Jeon <esjeon@hyunmu.am>
2025-03-06 10:30:30 +0900

    preliminary websocket implemenataion

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,37 @@
 import * as crypto from 'node:crypto';
-import EventEmitter from 'node:events';
 import { readFile } from 'node:fs/promises';
 import * as http from 'node:http';
-import { ListenOptions } from 'node:net';
+import { AddressInfo, ListenOptions } from 'node:net';
 import * as path from 'node:path';
 import { WebSocket, WebSocketServer } from 'ws';
 
 const DEFAULT_HTTP_PORT = 3000;
 const MAIN_HTML_PATH = '../static/main.html'
 
+class WSSession {
+  public static generateId(): string {
+    const timestamp = Date.now().toString(16);
+    const randomBytes = crypto.randomBytes(8).toString('hex');
+    return `${timestamp}-${randomBytes}`;
+  }
+
+  public id: string;
+  public iceCandidates: RTCIceCandidate[];
+
+  constructor(
+    public sock: WebSocket,
+  ) {
+    this.id = WSSession.generateId();
+    this.iceCandidates = []
+  }
+}
+
 export class MaruServer {
   public http: http.Server;
   public ws: WebSocketServer;
 
   public validKeys: Set<string>;
+  public sessions: Map<string, WSSession>;
 
   constructor() {
     const self = this;
@@ -32,12 +50,13 @@ export class MaruServer {
     this.ws.addListener('connection', (sock, req) => self.onWSConnect(sock, req));
 
     this.validKeys = new Set(['secret1', 'secret2', 'secret3']);
+    this.sessions = new Map();
   }
 
   public async listen(opts?: ListenOptions): Promise<void> {
     if (!opts) opts = {};
     if (!opts.port) opts.port = DEFAULT_HTTP_PORT;
-    if (!opts.host) opts.host = 'localhost';
+    if (!opts.host) opts.host = '127.0.0.1';
 
     await new Promise((resolve, reject) => {
       try {
@@ -46,7 +65,8 @@ export class MaruServer {
         reject(e);
       }
     });
-    console.log(`Server running at http://${opts.host}:${opts.port}/`);
+    const addrInfo = this.http.address() as AddressInfo;
+    console.log(`Server running at http://${addrInfo.address}:${addrInfo.port}/`);
   }
 
   private async onHttpRequest(req: http.IncomingMessage, resp: http.ServerResponse): Promise<void> {
@@ -65,7 +85,41 @@ export class MaruServer {
     }
   }
 
-  private async onWSConnect(sock: WebSocket, req: http.IncomingMessage): Promise<void> {
-    // TODO: register a new session
+  private verifyClientKey(key: string): boolean {
+    return this.validKeys.has(key);
+  }
+  
+  private onWSConnect(sock: WebSocket, req: http.IncomingMessage): void {
+    const self = this;
+
+    const session = new WSSession(sock);
+    this.sessions.set(session.id, session);
+
+    sock.addEventListener('message', (ev) => self.onWSMessage(session, ev));
+    sock.addEventListener('close', (ev) => self.onWSDisconnect(session, ev));
+    sock.addEventListener('error', (ev) => console.error("WS: error: %", ev));
+
+    console.info('WS: sessoin opened: session=%s', session.id)
+  }
+  
+  private onWSMessage(session: WSSession, ev: WebSocket.MessageEvent): void {
+    try {
+      const data = JSON.parse(ev.data as string);
+      
+      if (!data.key || !this.verifyClientKey(data.key))
+        throw new Error('invalid key');
+
+      if (data.iceCandidate)
+        session.iceCandidates.push(data.iceCandidate);
+    } catch(error) {
+      console.error('WS: error in session: session=% error=%s', session.id, error);
+      session.sock.send(JSON.stringify({ error }));
+      session.sock.close();
+    }
+  }
+
+  private onWSDisconnect(session: WSSession, ev: WebSocket.CloseEvent): void {
+    console.info('WS: sessoin closed: session=%s reason=%s code=%s', session.id, ev.reason, ev.code);
+    this.sessions.delete(session.id);
   }
 }
